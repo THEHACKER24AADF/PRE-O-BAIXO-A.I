586334f001ee6badbe1ce63ca88e826a665a2618
Matheus <massarandubadejesus@gmail.com>
2026-05-31 11:12:52 -0300

    feat: configuracao do painel de administracao e registro dos modelos Produto e HistoricoPreco

--- buscador/admin.py
+++ buscador/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Produto, HistoricoPreco
 
-# Register your models here.
+# Registrando as tabelas para elas aparecerem no painel
+admin.site.register(Produto)
+admin.site.register(HistoricoPreco)
